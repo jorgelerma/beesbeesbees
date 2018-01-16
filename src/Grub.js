7d30f7bd19e7334c37afb25ef6f5ac0ebaa41ac6
jorgelerma <jorge@jorgelerma.com>
2018-01-15 18:48:15 -0600

    Create Grub class

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,12 @@
-var Grub = function() {
+var Grub = function(age,color,food) {
+
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
 };
 
+Grub.prototype.eat = function(){
+
+}
+
+//var myGrub = new Grub(0,'pink','jelly');
